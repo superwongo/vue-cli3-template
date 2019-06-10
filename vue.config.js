c57c921015c7f9b6e44f2102e2bde627a94e1875
Administrator <wangchao_198912@126.com>
2019-06-10 16:14:20 +0800

    新增图片解析、打包分析

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const CompressionPlugin = require('compression-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path')
 
 module.exports = {
   chainWebpack: config => {
@@ -7,7 +9,40 @@ module.exports = {
       args[0]['process.env'].BASE_URL = JSON.stringify(process.env.BASE_URL)
       return args
     })
+
+    // #region svg-config
+    const svgRule = config.module.rule('svg') // 找到svg-loader
+    svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+    svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+    svgRule // 添加svg新的loader处理
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+
+    // 修改images loader 添加svg处理
+    const imagesRule = config.module.rule('images')
+    imagesRule.exclude.add(path.resolve('src/assets/icons'))
+    // #endregion svg-config
+
     if (process.env.NODE_ENV === 'production') {
+      // #region 图片压缩
+      config.module
+        .rule('images')
+        .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+        .use('img-loader')
+        .loader('img-loader').options({
+          plugins: [
+            require('imagemin-jpegtran')(),
+            require('imagemin-pngquant')({
+              quality: [0.75, 0.85]
+            })
+          ]
+        })
+      // #endregion
+
       // #region 启用GZip压缩
       config
         .plugin('compression')
@@ -20,7 +55,6 @@ module.exports = {
           cache: true
         })
         .tap(args => { })
-
       // #endregion
 
       // #region 忽略生成环境打包的文件
@@ -56,6 +90,16 @@ module.exports = {
           return args
         })
       // #endregion
+
+      // #region 分析打包体积
+      if (process.env.IS_ANALYZE) {
+        config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
+          {
+            analyzerMode: 'static'
+          }
+        ])
+      }
+      // #endregion 分析打包体积
     }
   }
 }
